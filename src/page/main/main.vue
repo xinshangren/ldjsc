29b5ce130f53d01fe440507474fe792910af30f6
sxctc123456/sxctc_zhupengfei <13620614683@139.com>
2019-12-22 14:58:34 +0800

    a

--- src/page/main/main.vue
+++ src/page/main/main.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
-    <headlineVue v-if="tabid==0"  style="overflow-y:auto;overflow-x:hidden;margin-top:55px;">
+    <headlineVue v-if="tabid==0" style="overflow-y:auto;overflow-x:hidden;margin-top:55px;">
     </headlineVue>
-    <dingbanVue  v-if="tabid==2" style="overflow-y:auto;overflow-x:hidden;margin-top:55px;">
+    <dingbanVue v-if="tabid==2" style="overflow-y:auto;overflow-x:hidden;margin-top:55px;">
     </dingbanVue>
-        <homeVue  v-if="tabid==4" style="overflow-y:auto;overflow-x:hidden;margin-top:55px;padding-bottom:55px;">
-        </homeVue>
-    
+    <homeVue v-if="tabid==4" style="overflow-y:auto;overflow-x:hidden;margin-top:55px;padding-bottom:55px;">
+    </homeVue>
 
-    <div
-      style="z-index: 2; display: flex;position: fixed;bottom: 0px;background: #ffffff;width: 100%;height: 55px;"
-    >
+
+    <div style="z-index: 2; display: flex;position: fixed;bottom: 0px;background: #ffffff;width: 100%;height: 55px;">
       <div class="main_item" id="0">
         <img class="main_item_img" v-bind:src="tabImage[0]" />
         <div class="main_item_noSelect">头条</div>
@@ -20,10 +18,7 @@
         <div class="main_item_noSelect">督办</div>
       </div>
       <div class="main_item" id="4" style="position: relative;width: 25%;">
-        <img
-          src="@/assets/img/tab_logo.png"
-          style="height: 54px;"
-        />
+        <img src="@/assets/img/tab_logo.png" style="height: 54px;" />
       </div>
       <div class="main_item" id="2">
         <img class="main_item_img" v-bind:src="tabImage[2]" />
@@ -38,48 +33,53 @@
 </template>
 
 <script>
-import Vue from "vue";
-import headlineVue from "@/page/main/headline.vue";
-import homeVue from "@/page/main/home.vue";
-import dingbanVue from "@/page/main/dingban.vue";
-import { Search } from "vant";
-import $ from "jquery";
-import { mainJs } from "../main/main.js";
-Vue.use(Search);
-export default {
-  name: "mainVue",
-  data() {
-    return {
-      tabid: 4,
-      seach_value: "",
-      tabImage: [
-        require("@/assets/img/icon_news.png"),
-        require("@/assets/img/icon_supervise.png"),
-        require("@/assets/img/icon_ding.png"),
-        require("@/assets/img/icon_hotline.png")
-      ]
-    };
-  },
-  mounted() {
-    var context = this;
-    $(".main_item").click(function() {
-      var id = $(this).attr("id");
-      context.changeTabStyle(id);
-    });
-  },
-  methods: {
-    //改变tab的状态和图片
-    changeTabStyle: function(tabid) {
-      this.tabid = tabid;
-      mainJs.changeTabStyle(tabid);
+  import Vue from "vue";
+  import headlineVue from "@/page/main/headline.vue";
+  import homeVue from "@/page/main/home.vue";
+  import dingbanVue from "@/page/main/dingban.vue";
+  import { Search } from "vant";
+  import $ from "jquery";
+  import { mainJs } from "../main/main.js";
+  Vue.use(Search);
+  export default {
+    name: "mainVue",
+    data() {
+      return {
+        tabid: 4,
+        seach_value: "",
+        tabImage: [
+          require("@/assets/img/icon_news.png"),
+          require("@/assets/img/icon_supervise.png"),
+          require("@/assets/img/icon_ding.png"),
+          require("@/assets/img/icon_hotline.png")
+        ]
+      };
+    },
+    mounted() {
+      var context = this;
+      $(".main_item").click(function () {
+        var id = $(this).attr("id");
+        context.changeTabStyle(id);
+      });
+    },
+    methods: {
+      //改变tab的状态和图片
+      changeTabStyle: function (tabid) {
+        if (tabid==1||tabid==3) {
+          this.$toast('功能开发中');
+        } else {
+          this.tabid = tabid;
+          mainJs.changeTabStyle(tabid);
+        }
+
+      }
+    },
+    components: {
+      homeVue,
+      headlineVue,
+      dingbanVue
     }
-  },
-  components: {
-    homeVue,
-    headlineVue,
-    dingbanVue
-  }
-};
+  };
 </script>
 
 <style scoped>
