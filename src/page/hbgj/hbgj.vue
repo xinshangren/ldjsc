de764d441bd90d486d5a8453aa0aa60631affedf
yzb <371330876@qq.com>
2019-12-24 09:51:42 +0800

    fff

--- src/page/hbgj/hbgj.vue
+++ src/page/hbgj/hbgj.vue
@@ -1,167 +1,68 @@
 <template>
-  <ul style="margin-top: 75px;">
-    <li class="li_class">
-      <div style="position:relative;" @click="intentTwoLevel(1)">
-        <div class="li_div_title1">空气质量</div>
-        
-      </div>
-      <div class="block_div_style" style="margin-top:5px;">
-        <div class="li_div_title2">本年优良天数总数</div>
+  <div style="margin-top:55px;">
+    <div class="ui-row-flex ui-whitespace">
+      <div class="ui-col ui-col">
         <div class="block_div_style">
-          <div class="li_div_title3_red">{{dataArrList[0]}}</div>
-          <div class="li_div_title2_red">天</div>
+          <img class="top_img"  src="../../assets/img/air_home_tab1selected.png" />
+          <div class="top_title_select1">空气监测</div>
         </div>
       </div>
-      <div class="block_div_style" style="margin-top:5px;">
-        <div class="li_div_title2">本年优良天数占比</div>
+      <div class="ui-col ui-col">
         <div class="block_div_style">
-          <div class="li_div_title3_red">{{dataArrList[1]}}</div>
-          <div class="li_div_title2_red">%</div>
+          <img class="top_img"  src="../../assets/img/air_home_tab2.png" />
+          <div class="top_title_noselect">空气监测</div>
         </div>
       </div>
-
-      <div style="position:relative;height:27px;margin-top:8px;">
-        <div class="zdgc_lk_bg">晋城市环保局</div>
-        <div class="zdgc_lk_bg_right">{{dataTime[0]}}</div>
-      </div>
-
-      <div class="hor_line_div"></div>
-      <div class="block_div_style" style="margin-top:15px;">
-        <div style="width:50%;display:flex;"  @click="toast()">
-          <img src="../../assets/img/icon_attention.png" class="bottom_img_style" />
-          <div class="bottom_div_font1">关注</div>
-        </div>
-        <div class="ver_line_div"></div>
-        <div style="width:50%;display:flex;"  @click="toast()">
-          <img src="../../assets/img/icon_duban.png" class="bottom_img_style" />
-          <div class="bottom_div_font2">督办</div>
-        </div>
-      </div>
-    </li>
-
-    <li class="li_class">
-      <div style="position:relative;" @click="intentTwoLevel(1)">
-        <div class="li_div_title1">网格监管</div>
-        
-      </div>
-      <div class="block_div_style" style="margin-top:5px;">
-        <div class="li_div_title2">监管污染源数量</div>
+      <div class="ui-col ui-col">
         <div class="block_div_style">
-          <div class="li_div_title3_red">{{dataArrList[2]}}</div>
-          <div class="li_div_title2_red">家</div>
+          <img class="top_img"  src="../../assets/img/air_home_tab3.png" />
+          <div class="top_title_noselect">空气监测</div>
         </div>
       </div>
-      <div class="block_div_style" style="margin-top:5px;">
-        <div class="li_div_title2">巡查次数</div>
-        <div class="block_div_style">
-          <div class="li_div_title3_red">{{dataArrList[3]}}</div>
-          <div class="li_div_title2_red">次</div>
-        </div>
-      </div>
-      <div class="block_div_style" style="margin-top:5px;">
-        <div class="li_div_title2">办结问题数</div>
-        <div class="block_div_style">
-          <div class="li_div_title3_red">{{dataArrList[4]}}</div>
-          <div class="li_div_title2_red">次</div>
-        </div>
-      </div>
-
-      <div style="position:relative;height:27px;margin-top:8px;">
-        <div class="zdgc_lk_bg">晋城市环保局</div>
-        <div class="zdgc_lk_bg_right">{{dataTime[2]}}</div>
-      </div>
-
-      <div class="hor_line_div"></div>
-      <div class="block_div_style" style="margin-top:15px;">
-        <div style="width:50%;display:flex;"  @click="toast()">
-          <img src="../../assets/img/icon_attention.png" class="bottom_img_style" />
-          <div class="bottom_div_font1">关注</div>
-        </div>
-        <div class="ver_line_div"></div>
-        <div style="width:50%;display:flex;"  @click="toast()">
-          <img src="../../assets/img/icon_duban.png" class="bottom_img_style" />
-          <div class="bottom_div_font2">督办</div>
-        </div>
-      </div>
-    </li>
-
-  </ul>
+    </div>
+  </div>
 </template>
 <script>
-  //import echarts from "echarts";
-  import { httpMethod } from "../../api/getData.js";
-  export default {
-    name: "zdgz",
-    beforeCreate() {
-      document.querySelector("body").setAttribute("style", "background:#F7F7F7");
-    },
-    activated() {//返回保留页面记录
-      document.querySelector("body").setAttribute("style", "background:#F7F7F7");
-    },
-    data() {
-      return {
-        dataArrList: ["0", "0", "0", "0", "0", "0"],
-        dataTime: []
+import echarts from "echarts";
+import { httpMethod } from "../../api/getData.js";
+export default {
+  name: "zdgz",
+  beforeCreate() {
+    document.querySelector("body").setAttribute("style", "background:#F7F7F7");
+  },
+  activated() {
+    //返回保留页面记录
+    document.querySelector("body").setAttribute("style", "background:#F7F7F7");
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+   //this.getListData();
+  },
+  methods: {
+    getListData: function() {
+      var params = {
+        targetIds: "58，59，61，62，63"
       };
-    },
-    mounted() {
-      this.getListData();
-    },
-    methods: {
-      toast: function () {
-        this.$toast('功能开发中');
-      },
-      getListData: function () {
-        var params = {
-          targetIds:
-            "58，59，61，62，63"
-        };
-
-        //获取数据
-        httpMethod
-          .findByTargetIds(params)
-          .then(res => {
-            console.log(res);
-            var code = res.success;
-            if (code == "1") {
-              var dataList = res.data;
-              this.dataArrList = [];
-              for (var ent in dataList) {
-                var targetId = dataList[ent].targetId;
-                var valuen = dataList[ent].valuen;
-                var updateTime = dataList[ent].updateTime;
-                updateTime = updateTime.substring(0, 11)
-                this.dataArrList.push(valuen);
-                this.dataTime.push(updateTime);
-              }
-              // console.log(this.dataArrList);
-            }
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      },
-      //跳转二级页面
-      intentTwoLevel: function (flag) {
-        // if (flag == 1) {
-        //   this.$router.push({
-        //     path: "/hbgj/hbgjkqzl/hbgjkqzl"
-        //   });
-        // } 
-        //else if (flag == 2) {
-        //   this.$router.push({
-        //     path: "/zdgz/zdgcjd"
-        //   });
-        // } else if (flag == 3) {
-        //   this.$router.push({
-        //     path: "/zdgz/zdgctzjd"
-        //   });
-        // }
-      }
+      //获取数据
+      httpMethod
+        .findByTargetIds(params)
+        .then(res => {
+          console.log(res);
+          var code = res.success;
+          if (code == "1") {
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-  };
+  }
+};
 </script>
 
 <style scoped>
-  @import "../../assets/css/zhibiao.css";
+@import "../../assets/css/frozenui.css";
+@import "../../page/hbgj/hbgj.css";
 </style>
