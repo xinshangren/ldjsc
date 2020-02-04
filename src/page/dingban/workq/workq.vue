78941d4d229a3331ae5e220a83dfbac9257cb818
yzb <371330876@qq.com>
2020-02-05 03:32:06 +0800

    ff

--- src/page/dingban/workq/workq.vue
+++ src/page/dingban/workq/workq.vue
@@ -18,13 +18,15 @@
       >查询</div>
     </div>
     <div style="background:#ffffff;">
-      <div v-for="(item,index) in list" :key="index" class="groupList" @click="goDetile(item)">
+      <div v-for="(item,index) in list" :key="index" class="groupList">
         <div style="display: flex;position: relative;">
-          <img src="../../../assets/img/group.png" class="groupLeftImg" />
-          <div style="line-height:33px;margin-left:12px;">{{item.name}}</div>
-          <img v-if="item.state==0" class="stateClass" src="../../../assets/img/zt1.png" />
-          <img v-if="item.state==1" class="stateClass" src="../../../assets/img/zt2.png" />
-          <img v-if="item.state==2" class="stateClass" src="../../../assets/img/zt3.png" />
+          <img src="../../../assets/img/group.png" class="groupLeftImg"  @click="goDetile(item)" />
+          <div style="display:flex;position: relative;width:80%;" @click="sjjq(item)">
+            <div style="line-height:33px;margin-left:12px;">{{item.name}}</div>
+            <img v-if="item.state==0" class="stateClass" src="../../../assets/img/zt1.png" />
+            <img v-if="item.state==1" class="stateClass" src="../../../assets/img/zt2.png" />
+            <img v-if="item.state==2" class="stateClass" src="../../../assets/img/zt3.png" />
+          </div>
         </div>
       </div>
       <img v-show="isshow" style="width:100%;" src="../../../assets/img/no-data.jpg" />
@@ -37,6 +39,7 @@ import { Search } from "vant";
 import $ from "jquery";
 import { httpMethod } from "../../../api/getData.js";
 import global_variable from "../../../api/global_variable.js";
+import dd from "dingtalk-jsapi";
 export default {
   name: "workqVue",
   data() {
@@ -44,7 +47,8 @@ export default {
       seach_value: "",
       list: [],
       isshow: false,
-      indexFlag: 0
+      indexFlag: 0,
+      corpId: ""
     };
   },
   beforeCreate() {
@@ -55,10 +59,11 @@ export default {
     document.querySelector("body").setAttribute("style", "background:#ffffff");
   },
   mounted() {
+    this.gojq();
     this.getGroupInfo(1);
-     var list=[];
+    var list = [];
     //  console.log(this.$parent.active);
-     
+
     //  this.$parent.getWorkGroupList(list);
   },
   methods: {
@@ -84,7 +89,7 @@ export default {
             if (datalist != null && datalist.length > 0) {
               this.list = this.list.concat(res.data);
               if (indexFlag == 0) {
-                 this.$emit("getWorkGroupList",this.list);
+                this.$emit("getWorkGroupList", this.list);
                 // this.$parent.getWorkGroupList(this.list);
               }
               indexFlag++;
@@ -93,17 +98,16 @@ export default {
             } else {
               this.list = [];
               this.isshow = true;
-               if (indexFlag == 0) {
-                 this.$emit("getWorkGroupList",this.list);
+              if (indexFlag == 0) {
+                this.$emit("getWorkGroupList", this.list);
                 // this.$parent.getWorkGroupList(this.list);
               }
             }
           } else if (res.code == "fail") {
-            var list=[];
-              this.$emit("getWorkGroupList",list);
+            var list = [];
+            this.$emit("getWorkGroupList", list);
             // this.$toast(res.message);
             this.isshow = true;
-            
           }
         })
         .catch(err => {
@@ -116,7 +120,74 @@ export default {
         name: "workqDeali",
         params: {
           entity: item
+        },
+        query: {
+          departId: "8a8180c970012108017003b8e60a0047",
+          num: "27",
+          menu: "111"
         }
+      });
+    },
+
+    gojq: function() {
+      var currentUrl = window.location.href; //当前页面地址
+      if (window.location.hash == "#/") {
+        currentUrl = currentUrl.substring(
+          0,
+          currentUrl.indexOf(window.location.hash)
+        );
+      }
+      console.log(currentUrl);
+      var params = {
+        currentUrl: currentUrl
+      };
+
+      httpMethod
+        .getConfig(params)
+        .then(res => {
+          if (res.success == "1") {
+            var data = JSON.parse(res.config);
+            this.corpId = data.corpId;
+            dd.ready(function() {
+              dd.config({
+                agentId: data.agentId,
+                corpId: data.corpId,
+                timeStamp: data.timeStamp,
+                nonceStr: data.nonceStr,
+                signature: data.signature,
+                jsApiList: ["biz.chat.toConversation", "runtime.info"]
+              });
+              dd.error(function(error) {
+                console.log("dd error: " + JSON.stringify(error));
+              });
+            });
+          } else if (res.success == "0") {
+            // this.error = true;
+          }
+        })
+        .catch(err => {
+          // this.$toast(err);
+        });
+    },
+    sjjq: function(entity) {
+      console.log(entity);
+      var self = this;
+      dd.ready(function() {
+        console.log(self.corpId);
+        console.log(entity.groupNumber);
+        dd.biz.chat.toConversation({
+          corpId: self.corpId, ////企业id,必须是用户所属的企业的corpid
+          chatId: entity.groupNumber, //会话Id
+          onSuccess: function(info) {
+            // console.log(self.corpId);
+            // console.log(self.entity.groupNumber);
+            // console.log("toConversation info: " + JSON.stringify(info));
+          },
+          onFail: function(e) {
+            console.log("toConversation fail: " + JSON.stringify(e));
+            //  this.$toast("系统出现问题，请稍后再试!");
+          }
+        });
       });
     }
   }
--- src/page/dingban/workq/workqDeali/workqDeali.vue
+++ src/page/dingban/workq/workqDeali/workqDeali.vue
@@ -30,7 +30,7 @@
 
     <!-- <div v-if="entity.state==2" class="workdealiBottomButton">申请</div> -->
 
-    <div v-if="entity.state==1" @click="sjjq()" class="workdealiBottomButton">点击进群</div>
+    <!-- <div v-if="entity.state==1" @click="sjjq()" class="workdealiBottomButton">点击进群</div> -->
   </div>
 </template>
 
@@ -63,37 +63,10 @@ export default {
   mounted() {
     this.itemEnti = this.$route.params.entity;
     console.log(JSON.stringify(this.$route.params.entity));
-    this.gojq();
+    // this.gojq();
     this.getGroupInfo(global_variable.userId, this.itemEnti.groupId);
   },
   methods: {
-    sjjq: function() {
-      var self = this;
-      dd.ready(function() {
-        // dd.runtime.info({
-        //   onSuccess: function(info) {
-        //     console.log("runtime info: " + JSON.stringify(info));
-        //   },
-        //   onFail: function(err) {
-        //     console.log("fail: " + JSON.stringify(err));
-        //   }
-        // });
-        console.log(self.corpId);
-        dd.biz.chat.toConversation({
-          corpId: self.corpId, ////企业id,必须是用户所属的企业的corpid
-          chatId: self.entity.groupNumber, //会话Id
-          onSuccess: function(info) {
-            console.log(self.corpId);
-            console.log(self.entity.groupNumber);
-            console.log("toConversation info: " + JSON.stringify(info));
-          },
-          onFail: function(e) {
-            console.log("toConversation fail: " + JSON.stringify(e));
-            //  this.$toast("系统出现问题，请稍后再试!");
-          }
-        });
-      });
-    },
     //获取群列表
     getGroupInfo: function(dingUserId, groupId) {
       var params = {
@@ -118,69 +91,69 @@ export default {
           // this.$toast(err);
         });
     },
-    
-      gojq: function () {
-        var currentUrl = window.location.href;//当前页面地址 
-        if (window.location.hash == "#/") {
-          currentUrl = currentUrl.substring(0, currentUrl.indexOf(window.location.hash));
-        }
-        var params = {
-          currentUrl: currentUrl
-        };
-
-        httpMethod
-          .getConfig(params)
-          .then(res => {
-            if (res.success == "1") {
-              var data = JSON.parse(res.config);
-              this.corpId = data.corpId;
-              dd.ready(function () {
-                dd.config({
-                  agentId: data.agentId,
-                  corpId: data.corpId,
-                  timeStamp: data.timeStamp,
-                  nonceStr: data.nonceStr,
-                  signature: data.signature,
-                  jsApiList: [
-                    'runtime.info',
-                    'biz.contact.choose',
-                    'device.notification.confirm',
-                    'device.notification.alert',
-                    'device.notification.prompt',
-                    'biz.ding.post',
-                    'biz.util.openLink',
-                    'device.audio',
-                    'device.audio.startRecord',
-                    'device.audio.stopRecord',
-                    'device.audio.translateVoice',
-                    'biz.ding.create',
-                    'biz.telephone.call',
-                    'biz.contact.complexPicker',
-                    'biz.util.open',
-                    'biz.chat.open',
-                    'biz.chat.pickConversation',
-                    'biz.user.get',
-                    'biz.util.uploadImage',
-                    'biz.chat.openSingleChat',
-                    'biz.ding.create',
-                    'biz.chat.toConversation'
-                  ]
-                });
-                dd.error(function (error) {
-                  alert('dd error: ' + JSON.stringify(error));
-                });
-              });
-
-            } else if (res.success == "0") {
-              this.error = true;
-            }
-
-          })
-          .catch(err => {
-            this.$toast(err);
-
-          });
+    gojq: function() {
+      var currentUrl = window.location.href; //当前页面地址
+      if (window.location.hash == "#/") {
+        currentUrl = currentUrl.substring(
+          0,
+          currentUrl.indexOf(window.location.hash)
+        );
       }
+      console.log(currentUrl);
+      var params = {
+        currentUrl: currentUrl
+      };
+
+      httpMethod
+        .getConfig(params)
+        .then(res => {
+          if (res.success == "1") {
+            var data = JSON.parse(res.config);
+            this.corpId = data.corpId;
+            dd.ready(function() {
+              dd.config({
+                agentId: data.agentId,
+                corpId: data.corpId,
+                timeStamp: data.timeStamp,
+                nonceStr: data.nonceStr,
+                signature: data.signature,
+                jsApiList: [
+                  "biz.chat.toConversation",
+                  "runtime.info",
+                ]
+              });
+              dd.error(function(error) {
+                console.log("dd error: " + JSON.stringify(error));
+              });
+            });
+          } else if (res.success == "0") {
+            // this.error = true;
+          }
+        })
+        .catch(err => {
+          // this.$toast(err);
+        });
+    },
+    sjjq: function() {
+      var self = this;
+      dd.ready(function() {
+        console.log(self.corpId);
+         console.log(self.entity.groupNumber);
+        dd.biz.chat.toConversation({
+          corpId: self.corpId, ////企业id,必须是用户所属的企业的corpid
+          chatId: self.entity.groupNumber, //会话Id
+          onSuccess: function(info) {
+            // console.log(self.corpId);
+            // console.log(self.entity.groupNumber);
+            // console.log("toConversation info: " + JSON.stringify(info));
+          },
+          onFail: function(e) {
+            console.log("toConversation fail: " + JSON.stringify(e));
+            //  this.$toast("系统出现问题，请稍后再试!");
+          }
+        });
+      });
+    }
   }
 };
 </script>
