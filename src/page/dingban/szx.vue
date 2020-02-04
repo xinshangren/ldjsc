0364cc407d8b1c76334cfd1e54bdf36f41c8aa60
monster0412 <zfkmwx@163.com>
2020-02-05 01:23:26 +0800

    sss

--- src/page/dingban/szx.vue
+++ src/page/dingban/szx.vue
@@ -188,66 +188,78 @@ export default {
     },
     //打电话
     goDetile(item) {
-      var ddd = this.corpId;
-      dd.ready(function() {
-        dd.biz.telephone.call({
-          users: [item.dingid], //用户列表，工号
-          corpId: ddd, //企业id
-          onSuccess: function() {},
-          onFail: function(e) {
-            alert("打电话错误" + JSON.stringify(e));
-          }
+      if(item.dingid != null){
+        var ddd = this.corpId;
+        dd.ready(function() {
+          dd.biz.telephone.call({
+            users: [item.dingid], //用户列表，工号
+            corpId: ddd, //企业id
+            onSuccess: function() {},
+            onFail: function(e) {
+              alert("打电话错误" + JSON.stringify(e));
+            }
+          });
         });
-      });
+      }else{
+        alert("该用户未在'晋城智能办公平台'");
+      }
     },
     //发消息
     goSms(item) {
-      var ddd = this.corpId;
-      dd.biz.chat.openSingleChat({
-        corpId: ddd, // 企业id,必须是用户所属的企业的corpid
-        userId: item.dingid, // 用户的工号
-        onSuccess: function() {},
-        onFail: function(e) {
-          alert("发消息" + JSON.stringify(e));
-        }
-      });
+      if(item.dingid != null){
+        var ddd = this.corpId;
+        dd.biz.chat.openSingleChat({
+          corpId: ddd, // 企业id,必须是用户所属的企业的corpid
+          userId: item.dingid, // 用户的工号
+          onSuccess: function() {},
+          onFail: function(e) {
+            alert("发消息" + JSON.stringify(e));
+          }
+        });
+      }else{
+        alert("该用户未在'晋城智能办公平台'");
+      }
     },
     goDing(item) {
-      var ddd = this.corpId;
-      dd.biz.ding.create({
-        users: [item.dingid], // 用户列表，工号
-        corpId: ddd, // 企业id
-        type: 1, // 附件类型 1：image  2：link
-        alertType: 2, // 钉发送方式 0:电话, 1:短信, 2:应用内
-        alertDate: { format: "yyyy-MM-dd HH:mm", value: "2019-12-23 08:00" },
-        attachment: {
-          images: [""]
-        }, // 附件信息
-        text: "", // 正文
-        bizType: 0, // 业务类型 0：通知DING；1：任务；2：会议；
-        confInfo: {
-          bizSubType: 0, // 子业务类型如会议：0：预约会议；1：预约电话会议；2：预约视频会议；（注：目前只有会议才有子业务类型）
-          location: "某某会议室", //会议地点；（非必填）
-          startTime: { format: "yyyy-MM-dd HH:mm", value: "2019-12-23 08:00" }, // 会议开始时间
-          endTime: { format: "yyyy-MM-dd HH:mm", value: "2019-12-23 08:00" }, // 会议结束时间
-          remindMinutes: 30, // 会前提醒。单位分钟-1：不提醒；0：事件发生时提醒；5：提前5分钟；15：提前15分钟；30：提前30分钟；60：提前1个小时；1440：提前一天；
-          remindType: 2 // 会议提前提醒方式。0:电话, 1:短信, 2:应用内
-        },
+      if(item.dingid != null){
+        var ddd = this.corpId;
+        dd.biz.ding.create({
+          users: [item.dingid], // 用户列表，工号
+          corpId: ddd, // 企业id
+          type: 1, // 附件类型 1：image  2：link
+          alertType: 2, // 钉发送方式 0:电话, 1:短信, 2:应用内
+          alertDate: { format: "yyyy-MM-dd HH:mm", value: "2019-12-23 08:00" },
+          attachment: {
+            images: [""]
+          }, // 附件信息
+          text: "", // 正文
+          bizType: 0, // 业务类型 0：通知DING；1：任务；2：会议；
+          confInfo: {
+            bizSubType: 0, // 子业务类型如会议：0：预约会议；1：预约电话会议；2：预约视频会议；（注：目前只有会议才有子业务类型）
+            location: "某某会议室", //会议地点；（非必填）
+            startTime: { format: "yyyy-MM-dd HH:mm", value: "2019-12-23 08:00" }, // 会议开始时间
+            endTime: { format: "yyyy-MM-dd HH:mm", value: "2019-12-23 08:00" }, // 会议结束时间
+            remindMinutes: 30, // 会前提醒。单位分钟-1：不提醒；0：事件发生时提醒；5：提前5分钟；15：提前15分钟；30：提前30分钟；60：提前1个小时；1440：提前一天；
+            remindType: 2 // 会议提前提醒方式。0:电话, 1:短信, 2:应用内
+          },
 
-        taskInfo: {
-          ccUsers: ["100", "101"], // 抄送用户列表，工号
-          deadlineTime: {
-            format: "yyyy-MM-dd HH:mm",
-            value: "2015-05-09 08:00"
-          }, // 任务截止时间
-          taskRemind: 30 // 任务提醒时间，单位分钟0：不提醒；15：提前15分钟；60：提前1个小时；180：提前3个小时；1440：提前一天；
-        },
+          taskInfo: {
+            ccUsers: ["100", "101"], // 抄送用户列表，工号
+            deadlineTime: {
+              format: "yyyy-MM-dd HH:mm",
+              value: "2015-05-09 08:00"
+            }, // 任务截止时间
+            taskRemind: 30 // 任务提醒时间，单位分钟0：不提醒；15：提前15分钟；60：提前1个小时；180：提前3个小时；1440：提前一天；
+          },
 
-        onSuccess: function() {
-          //onSuccess将在点击发送之后调用
-        },
-        onFail: function() {}
-      });
+          onSuccess: function() {
+            //onSuccess将在点击发送之后调用
+          },
+          onFail: function() {}
+        });
+      }else{
+        alert("该用户未在'晋城智能办公平台'");
+      }
     },
     toast: function() {
       this.$toast("功能开发中");
