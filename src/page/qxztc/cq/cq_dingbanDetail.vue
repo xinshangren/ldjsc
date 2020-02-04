0e564ee3ce4b017a8792ab2acae03177fab0a26e
yzb <371330876@qq.com>
2020-02-05 04:33:35 +0800

    fff

--- src/page/qxztc/cq/cq_dingbanDetail.vue
+++ src/page/qxztc/cq/cq_dingbanDetail.vue
@@ -114,12 +114,14 @@ export default {
     },
     gojq: function() {
       var currentUrl = window.location.href; //当前页面地址
-      if (window.location.hash == "#/" || window.location.hash == "#/qxztc/cq/cq_dingban") {
-        currentUrl = currentUrl.substring(
-          0,
-          currentUrl.indexOf(window.location.hash)
-        );
-      }
+      // if (window.location.hash == "#/" || window.location.hash == "#/qxztc/cq/cq_dingban") {
+      //   currentUrl = currentUrl.substring(
+      //     0,
+      //     currentUrl.indexOf(window.location.hash)
+      //   );
+      // }
+      var number=currentUrl.indexOf("#");
+      currentUrl = currentUrl.substring(0,number);
       console.log(currentUrl);
       var params = {
         currentUrl: currentUrl
