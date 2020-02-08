f9672ae6ae928efd86798123b4d7bddf3ebd3e36
houxinbin <595768894@qq.com>
2020-02-08 20:48:51 +0800

    修改bug6272

--- src/page/qyly/jq/jqdetile.vue
+++ src/page/qyly/jq/jqdetile.vue
@@ -121,7 +121,8 @@
         this.myPlayer = this.$video(myVideo, {
           //确定播放器是否具有用户可以与之交互的控件。没有控件，启动视频播放的唯一方法是使用autoplay属性或通过Player API。
           controls: true,
-          //自动播放属性,muted:静音播放
+          //自动播放属性,
+          muted:false,//静音播放
           autoplay: true,
           //建议浏览器是否应在<video>加载元素后立即开始下载视频数据。
           preload: "auto",
