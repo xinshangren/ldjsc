f534116cdb9af9d3bfb4a1d92707fbc1b726d2ad
monster0412 <zfkmwx@163.com>
2020-02-14 10:15:57 +0800

    sss

--- src/page/zdgz/zdgc/zdgc.vue
+++ src/page/zdgz/zdgc/zdgc.vue
@@ -1,6 +1,58 @@
+<style scoped>
+.menu_li_img {
+  height: 12px;
+  margin-top: 4px;
+  margin-right: 5px;
+  margin-left: 5px;
+}
+.menu_li_div {
+  color: #3098fb;
+  margin-left: 3px;
+  font-size: 15px;
+  width: 90%;
+}
+</style>
 <template>
   <div style="margin-top:54px;">
+    <div
+      id="menu_ul_id"
+      v-show="md_show"
+      style="width:43%; display:flex;position: fixed;background: #e8f4ff;top:101px;z-index:100;height:100%;left: -163px;"
+    >
+      <div style="left: 20px;background: #e8f4ff;top: 157px;z-index:100;">
+        <div @click=" (1)" style="display:flex;margin-top: 10px;background: #e8f4ff;">
+          <div class="menu_li_div">重点项目各阶段项目数量统计</div>
+          <img class="menu_li_img" src="../../../assets/img/data_arrow_right.png" />
+        </div>
+        <div @click="returnCom(2)" style="display:flex;margin-top:10px;background: #e8f4ff;">
+          <div class="menu_li_div">区县统计各阶段项目数量</div>
+          <img class="menu_li_img" src="../../../assets/img/data_arrow_right.png" />
+        </div>
+        <div @click="returnCom(3)" style="display:flex;margin-top:10px;background: #e8f4ff;">
+          <div class="menu_li_div">各区县实际投资额占比</div>
+          <img class="menu_li_img" src="../../../assets/img/data_arrow_right.png" />
+        </div>
+        <div @click="returnCom(4)" style="display:flex;margin-top:10px;background: #e8f4ff;">
+          <div class="menu_li_div">各区县实际投资额完成率</div>
+          <img class="menu_li_img" src="../../../assets/img/data_arrow_right.png" />
+        </div>
+        <div @click="returnCom(5)" style="display:flex;margin-top:10px;background: #e8f4ff;">
+          <div class="menu_li_div">各类型实际投资额占比</div>
+          <img class="menu_li_img" src="../../../assets/img/data_arrow_right.png" />
+        </div>
+        <div @click="returnCom(6)" style="display:flex;margin-top:10px;background: #e8f4ff;">
+          <div class="menu_li_div">各类型投资完成率</div>
+          <img class="menu_li_img" src="../../../assets/img/data_arrow_right.png" />
+        </div>
+      </div>
+      <img
+        id="show_menu_Id"
+        style="height:35px;position: absolute;top: 71px;right: -44px;"
+        src="../../../assets/img/data_list_menu.png"
+      />
+    </div>
     <van-tabs
+      id="tabs"
       @touchmove.prevent
       v-model="active"
       title-active-color="#2599e6"
@@ -8,6 +60,7 @@
       title-inactive-color="#333333"
       :sticky="true"
       line-width="75px"
+      style="width: 100%;"
       @change="tabsclick"
     >
       <van-tab title="总体情况">
@@ -30,7 +83,7 @@
         ref="child1"
         style="font-size:15px;"
       ></div>
-    </div> -->
+    </div>-->
     <div style="height: 50px;position: fixed;right: 0px;bottom: 11px;display:flex;">
       <div id="rightAreaDivId" style="display:none;">
         <div
@@ -91,7 +144,8 @@ export default {
     return {
       seach_value: "",
       active: 0,
-       currentView: "child1",
+      currentView: "child1",
+      md_show: false
     };
   },
   beforeRouteEnter(to, from, next) {
@@ -120,12 +174,37 @@ export default {
       "9",
       "总体情况"
     );
+     $("#show_menu_Id").click(function () {
+        if ($("#menu_ul_id").css('left') == '-163px') {
+          $("#menu_ul_id").animate({ 'left': '0px' }, 500);
+          $("#tabs").animate({ 'left': '156px' }, 500);
+          console.log($("#tjfx_tab"))
+          $(".van-sticky").animate({ 'left': '156px' }, 500);
+        } else {
+          $("#menu_ul_id").animate({ 'left': '-163px' }, 500);
+          $("#tabs").animate({ 'left': '0px' }, 500);
+          $(".van-sticky").animate({ 'left': '0px' }, 500);
+        }
+      });
+      $('body').click(function (e) {
+        if (e.target.id != 'show_menu_Id')
+          if ($("#menu_ul_id").css('left') == '-163px') {
+          } else {
+            $("#menu_ul_id").animate({ 'left': '-163px' }, 500);
+          $("#tabs").animate({ 'left': '0px' }, 500);
+          $(".van-sticky").animate({ 'left': '0px' }, 500);
+          }
+      })
   },
   methods: {
+    returnCom: function(x) {
+
+    },
     callPhone: function() {
       window.location.href = "tel://13935612128";
     },
     tabsclick: function(name, title) {
+      this.md_show = false;
       console.log(name);
       switch (parseInt(name)) {
         case 0:
@@ -135,7 +214,7 @@ export default {
             "9",
             "总体情况"
           );
-           this.currentView = "child1";
+          this.currentView = "child1";
           break;
         case 1:
           this.doAddAppLogList(
@@ -144,7 +223,7 @@ export default {
             "10",
             "项目列表"
           );
-           this.currentView = "child2";
+          this.currentView = "child2";
           break;
         case 2:
           this.doAddAppLogList(
@@ -153,7 +232,7 @@ export default {
             "11",
             "存在问题"
           );
-           this.currentView = "child3";
+          this.currentView = "child3";
           break;
         case 3:
           this.doAddAppLogList(
@@ -162,7 +241,8 @@ export default {
             "12",
             "统计分析"
           );
-           this.currentView = "child4";
+          this.md_show = true;
+          this.currentView = "child4";
           break;
         default:
           break;
