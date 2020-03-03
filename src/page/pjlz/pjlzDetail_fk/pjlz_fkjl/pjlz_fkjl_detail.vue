ab5fdd4e668aee038fdbf11b364d46d24365b329
monster0412 <zfkmwx@163.com>
2020-03-03 23:31:46 +0800

    sss

--- src/page/pjlz/pjlzDetail_fk/pjlz_fkjl/pjlz_fkjl_detail.vue
+++ src/page/pjlz/pjlzDetail_fk/pjlz_fkjl/pjlz_fkjl_detail.vue
@@ -16,7 +16,7 @@
     }
 </style>
 <template>
-    <div style="margin-top:54px;background:#f7f7f7;">
+    <div style="background:#f7f7f7;">
         <div id="content">
             <img width="250px" style='position: absolute; top: 37px;left: 69px;'
                 src="../../../../assets/img/detail_title.png" />
@@ -44,7 +44,6 @@
                             style="margin-left: 2px;border: 1px solid #9f9f9f;border-radius: 4px;width: 60%;font-size: 14px;min-height: 100px;">
                             <div style="margin:5px ;">
                                 {{feedback_obj.feedback_content}}
-
                             </div>
                         </div>
                     </div>
