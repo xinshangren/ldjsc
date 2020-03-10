e505877bf4dab1d3382025bf90983d156a42b137
yzb <371330876@qq.com>
2020-03-10 17:35:43 +0800

    ff

--- src/page/pjlz/pjlz.vue
+++ src/page/pjlz/pjlz.vue
@@ -419,7 +419,7 @@ export default {
       for (var i = 0; i < str.length; i++) {
         rs = rs + str.substr(i, 1).replace(pattern, "");
       }
-      
+        var rs = rs.replace(/[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig, "");
       this.sqjxmessage = rs;
       return str;
     },
